341587428bdc15e065b8a54d0c185017061acd97
Gavin Staniforth <gavshouse90@gmail.com>
2012-01-29 11:58:34 +0000

    Added Basic abstract controller

--- System/Controller.php
+++ System/Controller.php
@@ -2,5 +2,17 @@
 
 	abstract class Controller
 	{
+		protected $bootstrap;
+		
+		public function __construct(Bootstrap $bootstrap)
+		{
+			$this->$bootstrap = $bootstrap;
+		}
+		
+		protected function view($view)
+		{
+
+		}
+		
 		
 	}
