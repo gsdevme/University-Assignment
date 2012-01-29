844b88d7da431ab8b950ec6edae86180c9adc8e0
Gavin Staniforth <gavshouse90@gmail.com>
2012-01-29 13:08:29 +0000

    Abstract controller to assign views and then render() them

--- System/Controller.php
+++ System/Controller.php
@@ -2,17 +2,22 @@
 
 	abstract class Controller
 	{
+
 		protected $bootstrap;
-		
+
 		public function __construct(Bootstrap $bootstrap)
 		{
 			$this->bootstrap = $bootstrap;
 		}
-		
-		protected function view($view)
-		{
 
+		protected function view($view, array $args=null)
+		{
+			return ViewFactory::getInstance()->addView($view, $args);
 		}
-		
-		
+
+		protected function render()
+		{
+			return ViewFactory::getInstance()->render();
+		}
+
 	}
