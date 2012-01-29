1965e496f234acfcea707698428a090b77029e95
Gavin Staniforth <gavshouse90@gmail.com>
2012-01-29 11:20:01 +0000

    Added documentation

--- System/Router.php
+++ System/Router.php
@@ -16,13 +16,19 @@
 			$this->_defaultController = 'home';
 			$this->_defaultMethod = 'index';
 
+			// If there is no request parameter, else assign
 			$this->_route = ($request->getRequest() !== null) ? explode('/', $request->getRequest()) : array($this->_defaultController, $this->_defaultMethod);
 			
+			// If no method is within the request
 			if(count($this->_route) < 2){
 				array_push($this->_route, $this->_defaultMethod);
 			}
 		}
 
+		/**
+		 *
+		 * @return array  
+		 */
 		public function getRoute()
 		{
 			return ( array ) $this->_route;
