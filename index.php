306a9b8321f98f5c5b74a12428b7a9e3aedbd281
Gavin Staniforth <gavshouse90@gmail.com>
2012-01-29 10:12:35 +0000

    Changed include to be Uppercase

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
+
 	/**
 	* @author Gavin Staniforth
 	* @version 1.0, 29th January 2012
+	*
 	* This MVC Framework was created for use with PHP 5.2.6.
 	*
 	* This is the main target or any link, it includes some core configuration files and classes then 
 	* creates an instance of the request & router to kickstart the MVC
 	*/
 
-	require_once './bootstrap.php';
+	require_once './Bootstrap.php';
 
 	$bootstrap = Bootstrap::getInstance()->setupEnvironment()->setupAutoLoader()->configurePaths('~unn_w11025228');
 	$root = $bootstrap->getRoot();
