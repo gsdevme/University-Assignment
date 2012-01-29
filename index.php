84e21f563299ef8f59509443e7d5e390e0f1de39
Gavin Staniforth <gavshouse90@gmail.com>
2012-01-29 13:05:06 +0000

    Added ob_get_status detection, fixed spelling in comment, and added further comments

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	 * @author Gavin Staniforth
 	 * @version 1.0, 29th January 2012
 	 *
-	 * This MVC Framework was created for use with PHP 5.2.6 due to University servesrs being PHP 5.2.6
+	 * This MVC Framework was created for use with PHP 5.2.6 due to University servers being PHP 5.2.6
 	 *
 	 * This is the main target or any link, it includes some core configuration files and classes then 
 	 * creates an instance of the request & router to kickstart the MVC
@@ -19,22 +19,36 @@
 	unset($root);
 
 	try {
+		// Lets setup the environment for server/local then setup an autoloader and load configurations
 		$bootstrap = Bootstrap::getInstance()->setupEnvironment()->setupAutoLoader()->configurePaths('~unn_w11025228');
-		
+
+		// Lets add the locations to our bootstrap
 		$bootstrap->addFileLocation('System/');
 		$bootstrap->addFileLocation('System/Exceptions/');
 		$bootstrap->addFileLocation('Application/Controllers/');
 
 		$router = new Router(new Request($bootstrap));
-		
+
 		ControllerFactory::route($router->getRoute());
-		
+
 		// Remove variable from memory
 		unset($router);
 		unset($bootstrap);
 	} catch (Exception $e) {
-		echo '<pre>' . print_r($e->getMessage(), 1) . '</pre>';
-		echo '<pre>' . print_r($e->getCode(), 1) . '</pre>';
-		
-		echo '<pre>' . print_r($e->getPrevious(), 1) . '</pre>';
+		$obStatus = ob_get_status();
+
+		if (!empty($obStatus)) {
+			// Remove anything from the buffer
+			if (ob_get_length() > 0) {
+				ob_end_clean();
+			}
+		}
+
+		if ($e->getCode() == 404) {
+			ControllerFactory::notFound(array($e));
+			exit;
+		}
+
+		ControllerFactory::error(array($e));
 	}
+	
