de13ea4cdc2205714d903f23d928e292a7cab24b
Gavin Staniforth <gavshouse90@gmail.com>
2012-01-29 09:18:46 +0000

    Some little helper functions added, Sourced from my MVC 5.3.3 Framework https://github.com/JungleDevelopment/Panda

---
+++ functions.php
@@ -0,0 +1,38 @@
+<?php
+    /**
+     * This file is loaded by default within the index.php.
+     * 
+     * Its main purpose is to provide global functions across the application
+     */
+    
+
+    /**
+     * This function adds the purposed language construct detailed here
+     * https://wiki.php.net/rfc/ifsetor
+     * 
+     * @param mixed $value
+     * @param mixed $or
+     * @return mixed 
+     */
+    function ifsetor(&$value, $or=null)
+    {
+        return (isset($value)) ? $value : $or;
+    }
+
+    /**
+    * This function helps debugging during development
+    */
+    function pre()
+    {
+    	echo '<pre>'.print_r(func_get_args(), true).'</pre>';
+    }
+
+    /**
+     * Function which is "pass by reference" and calls isset() & empty()
+     * @param mixed $value
+     * @return bool 
+     */
+    function isValue(&$value)
+    {
+        return ( bool ) ((isset($value)) && (!empty($value)));
+    }    
