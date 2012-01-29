2581b29e98bb9e06ecc00f0b2332719ba1848b11
Gavin Staniforth <gavshouse90@gmail.com>
2012-01-29 13:07:44 +0000

    removed ucfirst() from method, added argument support and notFound method

--- System/ControllerFactory.php
+++ System/ControllerFactory.php
@@ -13,12 +13,44 @@
 
 			try {
 				$class = new ReflectionClass($controller);
-				$method = new ReflectionMethod($controller, ucfirst(array_shift($route)));
+				$method = new ReflectionMethod($controller, array_shift($route));
 
-				$method->invoke($class->newInstance(), null);
+				if (($method->isPublic()) && ($class->isInstantiable())) {
+					// is there any arguments ?					
+					if ((count($route) == 0) && ($method->getNumberOfRequiredParameters() == 0)) {
+						return $method->invoke($class->newInstance(Bootstrap::getInstance()), null);
+					}
+
+					// are we sending the correct amount of parameters ?
+					if (count($route) >= $method->getNumberOfRequiredParameters()) {
+						return $method->invokeArgs($class->newInstance(Bootstrap::getInstance()), $route);
+					}
+
+					throw new RouterException('The controller method was not sent the correct amount of parameters', 404);
+				}
+
+				throw new RouterException('The method is either not public or the class is not isInstantiable', 404);
 			} catch (ClassNotFoundException $e) {
-				pre($e);
+				// catch it for Line 37
 			}
+
+			throw new RouterException('The controller/method could not be found', 404, ifsetor($e));
+		}
+
+		/**
+		 * This method will create a NotFound object, which is used for show a 404 error
+		 * 
+		 * @param array $args
+		 * @return object
+		 */
+		public static function notFound(array $args=null)
+		{
+			$route = array_merge(array(
+				'NotFound',
+				'Index'
+				), $args);
+
+			return ControllerFactory::route($route);
 		}
 
 	}
