be824f9d2f7bf82b004d8a70a9d6fd507d077885
Gavin Staniforth <gavshouse90@gmail.com>
2012-01-29 10:11:58 +0000

    Changed the order of numyspace removal & clean()

--- System/Request.php
+++ System/Request.php
@@ -1,13 +1,20 @@
 <?php
 
+	/**
+	* @author Gavin Staniforth
+	* @version 1.0, 29th January 2012
+	*
+	* This is file is used 
+	*/
+
 	class Request
 	{
-	
 		
 		private $_request;
 
 		/**
-		*  
+		* Checks if the request_uri has a value, then performs str_replace(), substr(),
+		* cleans it before assigning to $_request
 		*
 		* @param Bootstrap $bootstrap 
 		*/
@@ -15,10 +22,11 @@
 		{
 			if (isValue($_SERVER['REQUEST_URI'])) {
                 $_SERVER['REQUEST_URI'] = substr(str_replace($_SERVER['SCRIPT_NAME'], null, $_SERVER['REQUEST_URI']), 1);
-                $_SERVER['REQUEST_URI'] = $this->_clean($_SERVER['REQUEST_URI']);
-
+                
                 // Remove Numyspace ID if any
                 $_SERVER['REQUEST_URI'] = ($bootstrap->isNumyspace()) ? str_replace($bootstrap->getNumyspaceId(), null, $_SERVER['REQUEST_URI']) : $_SERVER['REQUEST_URI'];
+
+                $_SERVER['REQUEST_URI'] = $this->_clean($_SERVER['REQUEST_URI']);
 
                 // If its still a value
                 if (isValue($_SERVER['REQUEST_URI'])) {
