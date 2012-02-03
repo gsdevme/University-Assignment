3c54b4571346c503b5fcb463f8d893a799710c46
Gavin Staniforth <gavshouse90@gmail.com>
2012-02-03 21:41:53 +0000

    Footer, Header & Meta elements

---
+++ Application/Elements/header.php
@@ -0,0 +1,17 @@
+<header>
+	<div class="container-960">
+		<h1><a href="<?php echo $url;?>home" title="Visit Newcastle">Visit Newcastle</a></h1>
+		<h2>Clubs, Pubs And Geordies</h2>
+
+		<nav>
+			<ul>
+				<li><a href="<?php echo $url;?>home" title="Homepage"><img src="<?php echo $img;?>home.png" alt="Homepage"/></a></li>
+				<li><a href="<?php echo $url;?>favourite" title="Favourites"><img src="<?php echo $img;?>favourite.png" alt="Favourite"/></a></li>
+				<li><a href="<?php echo $url;?>whatson">What's On</a></li>
+				<li><a href="<?php echo $url;?>places">Places</a></li>
+				<li><a href="<?php echo $url;?>fooddrink">Food &amp; Drink</a></li>
+				<li><a href="<?php echo $url;?>holidays">Holidays</a></li>
+			</ul>
+		</nav>	
+	</div>
+</header>
---
+++ Application/Elements/meta.php
@@ -0,0 +1,11 @@
+<meta charset="utf-8"/>
+<title><?php echo $title; ?></title>
+
+<link rel="shortcut icon" href="<?php echo $img; ?>favicon.ico" />	
+
+<link rel="stylesheet" href="<?php echo $css; ?>reset.css"/>
+<link rel="stylesheet" href="<?php echo $css; ?>style.css"/>
+
+<!--[if lt IE 9]>
+<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
