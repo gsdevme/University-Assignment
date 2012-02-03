106fb63f071a56563cdb0ce836c96e2a34678dd4
Gavin Staniforth <gavshouse90@gmail.com>
2012-02-03 13:10:53 +0000

    Added Error & NotFound

--- Application/Controllers/Error.php
+++ Application/Controllers/Error.php
@@ -1,10 +1,10 @@
 <?php
 
-	class NotFound extends Controller
+	class Error extends Controller
 	{
 
 		public function index(Exception $exception=null)
-		{
+		{			
 			$this->view('notfound', array('exception' => $exception)->render();
 		}
 
