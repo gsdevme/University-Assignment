9c6bf606ba0074269686bc44ade722392cf38460
Gavin Staniforth <gavshouse90@gmail.com>
2012-02-03 13:12:23 +0000

    Added docs, and added Library & Model methods

--- System/Controller.php
+++ System/Controller.php
@@ -1,15 +1,56 @@
 <?php
 
+	/**
+	 * @author Gavin Staniforth
+	 * @version 1.0, 3rd Feburary 2012
+	 *
+	 * This class is an abstract controll for all user defined controllers
+	 */	
 	abstract class Controller
 	{
 
-		protected $bootstrap;
+		protected $bootstrap, $controller, $method;
 
-		public function __construct(Bootstrap $bootstrap)
+		/**
+		* 
+		*
+		* @param Bootstrap $bootstrap
+		* @param string $controller
+		* @param string $method
+		*/
+		public function __construct(Bootstrap $bootstrap, $controller, $method)
 		{
 			$this->bootstrap = $bootstrap;
+			$this->controller = $controller;
+			$this->method = $method;
 		}
 
+		/**
+		* 
+		*
+		* @param string $model This is the name of the model
+		* @param array $args
+		* @return object
+		*/
+		protected function model($model, array $args=null)
+		{
+			return Factory::model($model, $args);
+		}
+
+		/**
+		*
+		*
+		* @param string $library This is the name of the library
+		* @param array $args
+		* @return object
+		*/
+		protected function library($library, array $args=null)
+		{
+			return Factory::library($library, $args);
+		}
+
+		/**
+		*/
 		protected function view($view, array $args=null)
 		{
 			return ViewFactory::getInstance()->addView($view, $args);
