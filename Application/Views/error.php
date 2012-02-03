8a39fdd81353dc61f3b795383da72c4466aa3e87
Gavin Staniforth <gavshouse90@gmail.com>
2012-02-03 21:42:00 +0000

    Error view

---
+++ Application/Views/error.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>		
+		<?php $this->element('meta'); ?>
+	</head>
+	
+	<body>		
+		<h1>Error</h1>
+
+		<?php pre($exception->getMessage()); ?>
+	</body>
+</html>
