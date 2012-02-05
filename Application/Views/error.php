55e9f764b55c6f2637aada5a666edf6c8fa459df
Gavin Staniforth <gavshouse90@gmail.com>
2012-02-05 05:50:44 +0000

    Added trace() output and simple NotFound

--- Application/Views/error.php
+++ Application/Views/error.php
@@ -1,14 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>		
+		<!-- Lets get our CSS and charset sorted. -->
 		<?php $this->element('meta'); ?>
 	</head>
 	
-	<body>		
-		<h1>PHP Error</h1>
+	<body>	
+		<!---
+			THIS VIEW IS ONLY USED FOR DISPLAYING EXCEPTIONS, NOTHING MORE
+		-->
 
-		<p><?php pre($exception->getMessage()); ?></p>
+		<!-- load our header element, same across all pages -->
+		<?php $this->element('header'); ?>
 
-		<?php echo '<pre>' . print_r($exception, true) . '</pre>'; ?>
+		<!-- wrap our homepage within a 960 and mainpage -->
+		<div class="container-960 mainpage">
+			<?php if(isset($exception)): ?>
+				<h2>Line <?php echo $exception->getLine(); ?> Trigged: <?php echo $exception->getMessage(); ?></h2>
+				<h4>File: <?php echo $exception->getFile(); ?></h4>
+
+				<table>
+					<?php foreach((array)$exception->getTrace() as $trace): ?>
+						<tr>
+							<?php foreach($trace as $property => $value): ?>
+								<td><?php echo $value; ?></td>							
+							<?php endforeach; ?>						
+						<tr>
+					<?php endforeach; ?>
+				</table>
+			<?php endif; ?>
+		</div>
+
+		<!-- all thats left is our footer now.. -->
+		<?php $this->element('footer'); ?>
 	</body>
 </html>
--- Application/Views/notfound.php
+++ Application/Views/notfound.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>		
+		<!-- Lets get our CSS and charset sorted. -->
 		<?php $this->element('meta'); ?>
 	</head>
 	
 	<body>	
+		<!---
+			THIS VIEW IS ONLY USED FOR DISPLAYING EXCEPTIONS, NOTHING MORE
+		-->
+
+		<!-- load our header element, same across all pages -->
 		<?php $this->element('header'); ?>
 
-		<h1>404 - Not Found</h1>
+		<!-- wrap our homepage within a 960 and mainpage -->
+		<div class="container-960 mainpage">
+			<h1>404 - Not Found</h1>
+			<p>It looks like this page doesn't exit !</p>
+		</div>
+
+		<!-- all thats left is our footer now.. -->
+		<?php $this->element('footer'); ?>
 	</body>
 </html>
