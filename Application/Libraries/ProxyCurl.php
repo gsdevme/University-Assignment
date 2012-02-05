ce140fba7a9be90c07b551e28732b7b15a447681
Gavin Staniforth <gavshouse90@gmail.com>
2012-02-05 18:04:43 +0000

    ProxyCurl class to curl a URL then return the result as a string

--- Application/Libraries/ProxyCurl.php
+++ Application/Libraries/ProxyCurl.php
@@ -1,7 +1,22 @@
 <?php 
 
+	/**
+	 * @author Gavin Staniforth <Email:gsdev@me.com> <Arpanet:http://gsdev.me> @gsphpdev
+	 * @version 1.0, 5th February 2012
+	 *
+	 * This class is used to just proxy through a URL, its purpose was to obtain the 
+	 * images for the holidays using Javascript
+	 */	
 	class ProxyCurl
 	{
+
+		/**
+		*  
+		*
+		* @static
+		* @param string $url
+		* @return string
+		*/
 		public static function get($url)
 		{
 			$curl = curl_init();
