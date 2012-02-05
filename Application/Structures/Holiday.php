20d29b37c5b162ef5f07680fb7be30ffc0c876a7
Gavin Staniforth <gavshouse90@gmail.com>
2012-02-05 01:18:26 +0000

    Holiday object which ensures I'm not sending a XML object to the view. also cleans up the title

---
+++ Application/Structures/Holiday.php
@@ -0,0 +1,44 @@
+<?php
+
+	class Holiday
+	{
+
+		private $_id, $_title, $_offer, $_link, $_description, $_pubDate, $_guid;
+
+		public function __construct($item)
+		{
+			foreach($item as $property => $value){
+				$name = '_' . $property;
+
+				if(property_exists($this, $name)){
+					$this->$name = $value;
+				}			
+			}
+
+			/*
+			* Lets try and parse out the offer from the title... (could explode it but wont be as fast)
+			* 
+			* Since some of the XML titles have | and some have - we need to figure it out... "sigh :(".. 
+			* i would think Regex is too much overhead though
+			*/
+			$this->_offer = trim(substr(strrchr($this->_title, '|'), 2));
+
+			if(empty($this->_offer)){
+				$this->_offer = trim(substr(strrchr($this->_title, '-'), 2));
+			}
+
+			// Now lets remove the offer sttring from the title
+			$this->_title = trim(substr(str_replace($this->_offer, null, $this->_title), 0, -3));
+
+			// Lets create an integer ID based upon the GUID.... (which it if its a real one shouldn't change)
+			if(!isset($this->_id)){
+				$this->_id = sprintf('%u', crc32($this->_guid));
+			}
+		}
+
+		public function __get($name)
+		{
+			$name = '_' . $name;
+			return ((isset($this->$name)) && (!empty($this->$name))) ? $this->$name : null;
+		}
+	}
