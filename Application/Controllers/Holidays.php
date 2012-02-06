f4446a5536aaf7722711ef1068c573871b4c969a
Gavin Staniforth <gavshouse90@gmail.com>
2012-02-06 04:34:29 +0000

    Added a holiday view, removed the home view then added a holiday controller

--- Application/Controllers/Holidays.php
+++ Application/Controllers/Holidays.php
@@ -1,13 +1,15 @@
 <?php
 
-	class Home extends AbstractController
+	class Holidays extends AbstractController
 	{
 
 		public function index()
 		{					
-			$this->title = 'Home - Second Rate Holidays';
+			$this->title = 'Holidays - Second Rate Holidays';
 			$this->description = 'All inclusive holidays from egypt to jamaica, we provide some of the greatest holidays the world has the offer.';
 			$this->keywords = 'home, second, rate, holidays, package, deals, flight, inclusive, world';
+
+			array_push($this->breadcrumb, array('holidays', 'Holidays'));
 
 			$this->view('holidays', array(
 				'holidays' => Factory::library('HolidayXML')->getHolidayObjects(),
