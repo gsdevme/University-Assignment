4fe6d92e2a0ba57faebd5a52d4217a1bc958ed4d
Gavin Staniforth <gavshouse90@gmail.com>
2012-04-23 04:23:07 +0100

    Completed documentation

--- Application/Controllers/AbstractController.php
+++ Application/Controllers/AbstractController.php
@@ -1,9 +1,23 @@
 <?php
 
+	/**
+	 * @author Gavin Staniforth <Email:gsdev@me.com> <Arpanet:http://gsdev.me> @gsphpdev
+	 * @version 1.0, 23rd April 2012
+	 *
+	 * Abstract controller for the application
+	 */
 	abstract class AbstractController extends Controller
 	{
+		// Protected properties across the contollers, used within the View overload
 		protected $title, $description, $keywords, $breadcrumb, $url, $css, $img, $js;
 		
+		/**
+		 * Get some properties set
+		 * 
+		 * @param Bootstrap $bootstrap
+		 * @param string $controller
+		 * @param string $method 
+		 */		
 		public function __construct(Bootstrap $bootstrap, $controller, $method)
 		{
 			$this->url = $bootstrap->getUrl();
@@ -18,6 +32,12 @@
 			parent::__construct($bootstrap, $controller, $method);
 		}
 		
+		/**
+		 * View overload to pass some key information
+		 * 
+		 * @param  string $view 
+		 * @param  array $args=null
+		 */
 		protected function view($view, array $args=null)
 		{
 			return parent::view($view, array_merge((array)$args, array(
