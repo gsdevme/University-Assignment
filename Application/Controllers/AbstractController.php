dfb6e95f8eba21a59ca566395a75bdf96ef9a289
Gavin Staniforth <gavshouse90@gmail.com>
2012-04-24 08:05:50 +0100

    Added user property, and session validation

--- Application/Controllers/AbstractController.php
+++ Application/Controllers/AbstractController.php
@@ -9,7 +9,7 @@
 	abstract class AbstractController extends Controller
 	{
 		// Protected properties across the contollers, used within the View overload
-		protected $title, $description, $keywords, $breadcrumb, $url, $css, $img, $js;
+		protected $title, $description, $keywords, $breadcrumb, $url, $css, $img, $js, $user;
 		
 		/**
 		 * Get some properties set
@@ -28,8 +28,46 @@
 			$this->breadcrumb = array(
 				array('home', 'Home'),
 			);
+
+			// lets start a session, but just make sure we aint starting one which is already made
+			if(!isset($_SESSION)){
+				session_start();
+			}
+
+			// Is there any users logged in?
+			if(isset($_SESSION['user'], $_SESSION['__ip_checksum'])){
+				// Lock sessions to IPs
+				if($_SESSION['__ip_checksum'] == sprintf('%u', crc32($bootstrap->getServerParam('remote_addr')))){
+
+					$this->user = ( object )$_SESSION['user'];
+
+					/**
+					 * We only really need to regenerate the session ID if there is a user,
+					 * there is no security advant to creating loads of sessions where they are empty, 
+					 * your just wasting 4k on your harddrive aswell as wasting headers in the HTTP request
+					 */
+					session_regenerate_id();
+				}else{
+					// Reset the session to be a blank array :) as the IP has change... perhaps stolen!!
+					$_SESSION = array();
+				}
+			}
+
+			// Register IP
+			$_SESSION['__ip_checksum'] = sprintf('%u', crc32($bootstrap->getServerParam('remote_addr')));		
 			
 			parent::__construct($bootstrap, $controller, $method);
+		}
+
+		/**
+		 * Simple method to just prepend the value & redirect while also killing the script
+		 * 
+		 * @param  string $url
+		 */
+		protected function redirect($url)
+		{
+			header('location: ' . $this->url . $url);
+			exit(0);
 		}
 		
 		/**
@@ -49,7 +87,8 @@
 				'img' => $this->img,
 				'url' => $this->url,
 				'js' => $this->js,
-				'controller' => $this->controller
+				'controller' => $this->controller,
+				'user' => $this->user
 			)));
 		}
 	}
