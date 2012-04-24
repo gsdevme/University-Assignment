721d6fdf7923e79bd5e2616aa21fcdfe922a3889
Gavin Staniforth <gavshouse90@gmail.com>
2012-04-24 09:09:36 +0100

    Added some headings

---
+++ Application/Controllers/Places.php
@@ -0,0 +1,26 @@
+<?php
+
+	/**
+	 * @author Gavin Staniforth <Email:gsdev@me.com> <Arpanet:http://gsdev.me> @gsphpdev
+	 * @version 1.0, 23rd April 2012
+	 *
+	 * Just an example page really, not needed but really you need more then 1 page
+	 */
+	class Places extends AbstractController
+	{
+
+		/**
+		 * Index page
+		 */
+		public function index()
+		{					
+			$this->title = 'Places - Second Rate Holidays';
+			$this->description = 'All inclusive holidays from egypt to jamaica, we provide some of the greatest holidays the world has the offer.';
+			$this->keywords = 'home, second, rate, holidays, package, deals, flight, inclusive, world';
+
+			array_push($this->breadcrumb, array('places', 'Places'));
+
+			$this->view('places')->render();
+		}
+
+	}
