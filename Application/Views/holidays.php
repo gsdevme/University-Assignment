4593b73c559c22c32fe99ad8ff2646807035c763
Gavin Staniforth <gavshouse90@gmail.com>
2012-04-25 07:49:40 +0100

    Renamed holidays

--- Application/Views/holidays.php
+++ Application/Views/holidays.php
@@ -20,7 +20,7 @@
 				<?php endif; ?>
 
 				<!-- Lets now load our holiday list element -->
-				<?php if(isset($holidays)) for($holidays;$holidays->valid();$holidays->next()): ?>
+				<?php if(isset($holidays)): for($holidays;$holidays->valid();$holidays->next()): ?>
 					<!-- Holiday class, with Image, Title, PubDate, Offer and Description -->
 					<div class="holidays">
 						<a href="<?php echo $holidays->current()->guid; ?>" target="_blank" title="<?php echo $holidays->current()->title; ?>">
@@ -30,7 +30,11 @@
 						<div class="text">
 							<?php if(isset($user)): ?>
 								<div class="fav">
-									<a href="<?php echo $url;?>favourites/toggle/<?php echo urlencode(base64_encode($holidays->current()->guid)); ?>" title="Add to Favourites"><img src="<?php echo $img; ?>add.png" alt="Add to Favourites"></a>
+									<?php if(isset($savedHolidays, $savedHolidays[$holidays->current()->id])): ?>
+										<a href="<?php echo $url;?>favourites/toggle/<?php echo urlencode(base64_encode($holidays->current()->guid)); ?>" title="Remove from Favourites"><img src="<?php echo $img; ?>remove.png" alt="Remove from Favourites"></a>
+									<?php else: ?>
+										<a href="<?php echo $url;?>favourites/toggle/<?php echo urlencode(base64_encode($holidays->current()->guid)); ?>" title="Add to Favourites"><img src="<?php echo $img; ?>add.png" alt="Add to Favourites"></a>
+									<?php endif; ?>
 								</div>
 							<?php else: ?>
 								<div class="fav">
@@ -43,8 +47,11 @@
 
 							<p><?php echo $holidays->current()->description; ?></p>
 						</div>
-					</div>					
+					</div>				
 				<?php endfor; ?>
+				<?php else: ?>
+					<p>No Holidays saved.</p>
+				<?php endif; ?>
 			</div>
 
 			<div id="right">
