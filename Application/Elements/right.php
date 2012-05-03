052077977c29681753e21efc7068a5683d134488
Gavin Staniforth <gavshouse90@gmail.com>
2012-05-03 01:20:46 +0100

    Added font increase & w3schools stuff

--- Application/Elements/right.php
+++ Application/Elements/right.php
@@ -26,5 +26,27 @@
 	</div>
 </div>
 
-<h3><a href="<?php echo $url;?>readme.txt">readme.txt for the assignment is provided here</a></h3>
-<h3><a href="<?php echo $url;?>git.log.txt">GIT Commit log here</a></h3>
+<div class="fontsize">
+	<ul>
+
+		<li><a href="#increase" onclick="font.increase();return false;">Increase Font Size</a></li>
+		<li><a href="#decrease" onclick="font.decrease();return false;">Decrease Font Size</a></li>
+	</ul>
+</div>
+
+<p>
+    <a href="http://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fwww.numyspace.co.uk%2F~unn_w11025228%2Fhome&profile=css3&usermedium=all&warning=1&vextwarning=&lang=en">
+        <img style="border:0;width:88px;height:31px"
+            src="http://jigsaw.w3.org/css-validator/images/vcss"
+            alt="Valid CSS!" />
+    </a>
+</p>
+
+<p>
+    <a href="http://validator.w3.org/check?uri=http%3A%2F%2Fwww.numyspace.co.uk%2F~unn_w11025228%2Fhome&charset=%28detect+automatically%29&doctype=Inline&group=0"><img
+      src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
+  </p>
+
+<h3><a href="<?php echo $url;?>readme.txt">readme.txt for the assignment is provided here</a></h3><br/>
+<h3><a href="<?php echo $url;?>docs/namespace-None.html">apigen documentation</a></h3><br/>
+<h3><a href="<?php echo $url;?>git.log.txt">GIT Commit log for the assignment is provided here</a></h3>
